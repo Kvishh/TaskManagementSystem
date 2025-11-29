0bb7377fa9c83e01e146ee616c6e2bd5559fc302
Vish <kelvishvargas23@gmail.com>
2025-11-29 13:10:49 +0800

    Added sqlite jdbc dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module org.example.taskmanagementsystem {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens org.example.taskmanagementsystem to javafx.fxml;
     exports org.example.taskmanagementsystem;
