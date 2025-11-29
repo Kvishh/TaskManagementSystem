72623a41634e347c03776036949689e9d919a052
Vish <kelvishvargas23@gmail.com>
2025-11-29 10:19:44 +0800

    init commit

---
+++ src/main/java/org/example/taskmanagementsystem/Main.java
@@ -0,0 +1,16 @@
+package org.example.taskmanagementsystem;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Scene scene = new Scene(new StackPane(new Label("Hello")), 300, 300);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
