efb339b1ec696fda0f9f0ca62eb14d49d73df133
Vish <kelvishvargas23@gmail.com>
2025-12-08 10:14:14 +0800

    Added tableview in the stage. Added the functionality to add rows to the table by the add task button. Result class now has two constructors, new one is for adding trash can image in the delete column

--- src/main/java/org/example/taskmanagementsystem/Main.java
+++ src/main/java/org/example/taskmanagementsystem/Main.java
@@ -4,14 +4,19 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.time.LocalDate;
+import java.util.Date;
 import java.util.Optional;
 
 public class Main extends Application {
@@ -27,6 +32,12 @@ public class Main extends Application {
         Scene scene = new Scene(root, 800, 600);//w=800, h=600
         Text mainHeader = new Text("TrackTask");
         Button addButton = new Button("+ Add new task");
+        TableView<Result> table = new TableView<>();
+        String url = "/org/example/taskmanagementsystem/styles/images/trashCanRegularFull.png";
+
+
+        buildTable(table, url);
+
         addButton.getStyleClass().add("addButton");
         addButton.setCursor(Cursor.HAND);
         addButton.setMinSize(100, 40);
@@ -41,20 +52,29 @@ public class Main extends Application {
         //scene.getStylesheets().add(getClass().getResource("stylesheet.css").toExternalForm());
 
         mainHeader.setFont(Font.font("Tahoma", 34));
-
         mainHeader.setFill(Color.WHITE);
 
         addButton.setOnMousePressed((e) -> {
             AddDialog addDialog = new AddDialog(new GridPane(10, 10), addButton);
 
             Optional<Result> result = addDialog.showAndWait();
-            result.ifPresent(res -> res.print());
+            result.ifPresent(res -> {
+                res.print();
+                table.getItems().add(new Result(res.getTask(),
+                        res.getStatus(),
+                        res.getPriority(),
+                        res.getDueDate(),
+                        new ImageView(new Image(getClass().getResourceAsStream(url), 24, 24, true, false))));
+                //STRUCTURE:                       Task       Status  Priority     Due Date      trash can image
+                //table.getItems().add(new Result("Breathe", "To do", "Urgent", LocalDate.now(), trashCanImage));
+            });
         });
 
         header.setPadding(new Insets(20));
         header.getChildren().addAll(addButton, spacer, mainHeader);
         header.setSpacing(50);
         root.setTop(header);
+        root.setCenter(table);
 
 //        mainHeader.setStyle("-fx-fill: blue");
         scene.getStylesheets().add(this.getClass().getResource("/org/example/taskmanagementsystem/styles/stylesheet.css").toExternalForm());
@@ -70,5 +90,34 @@ public class Main extends Application {
         stage.setTitle("TrackTask (Task Management System)");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void buildTable(TableView table, String url){
+        TableColumn done = new TableColumn("Done");
+        done.setResizable(false);
+        done.setMinWidth(103.3);
+        TableColumn task = new TableColumn("Task");
+        task.setCellValueFactory(new PropertyValueFactory<Result, String>("task"));
+        task.setResizable(false);
+        task.setMinWidth(220.7);
+        TableColumn status = new TableColumn("Status");
+        status.setCellValueFactory(new PropertyValueFactory<Result, String>("status"));
+        status.setResizable(false);
+        status.setMinWidth(123.3);
+        TableColumn priority = new TableColumn("Priority");
+        priority.setCellValueFactory(new PropertyValueFactory<Result, String>("priority"));
+        priority.setResizable(false);
+        priority.setMinWidth(123.3);
+        TableColumn dueDate = new TableColumn("Due date");
+        dueDate.setCellValueFactory(new PropertyValueFactory<Result, LocalDate>("dueDate"));
+        dueDate.setResizable(false);
+        dueDate.setMinWidth(123.3);
+        TableColumn delete = new TableColumn("Delete");
+        delete.setCellValueFactory(new PropertyValueFactory<Result, ImageView>("trashCanImage"));
+        delete.setResizable(false);
+        delete.setMinWidth(103.3);
+
+        table.getColumns().addAll(done, task, status, priority, dueDate, delete);
+        table.getItems().add(new Result("Finish final programming final project", "To do", "Urgent", LocalDate.now(), new ImageView(new Image(getClass().getResourceAsStream(url), 24, 24, true, false))));
     }
 }
--- src/main/java/org/example/taskmanagementsystem/Result.java
+++ src/main/java/org/example/taskmanagementsystem/Result.java
@@ -1,5 +1,8 @@
 package org.example.taskmanagementsystem;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 import java.time.LocalDate;
 
 public class Result {
@@ -7,12 +10,42 @@ public class Result {
     private String status;
     private String priority;
     private LocalDate dueDate;
+    private ImageView trashCanImage;
+            //= new ImageView(new Image("file:trashCanRegularFull.png", 32, 32, false, false));
 
-    public Result(String task,String status, String priority, LocalDate dueDate){
+    public Result(String task, String status, String priority, LocalDate dueDate, ImageView image){
         this.task = task;
         this.status = status;
         this.priority = priority;
         this.dueDate = dueDate;
+        this.trashCanImage = image;
+    }
+
+    public Result(String task, String status, String priority, LocalDate dueDate){
+        this.task = task;
+        this.status = status;
+        this.priority = priority;
+        this.dueDate = dueDate;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public ImageView getTrashCanImage() {
+        return trashCanImage;
     }
 
     void print(){
