183906bc232b46beee3f3a2cb0aae7cd7b7bbb14
Vish <kelvishvargas23@gmail.com>
2025-12-13 12:53:28 +0800

    Cleared the unused imports in the Main class

--- src/main/java/org/example/taskmanagementsystem/Main.java
+++ src/main/java/org/example/taskmanagementsystem/Main.java
@@ -1,25 +1,10 @@
 package org.example.taskmanagementsystem;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Cursor;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.time.LocalDate;
-import java.util.Optional;
 
 public class Main extends Application {
 
